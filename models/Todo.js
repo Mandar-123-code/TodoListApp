c9c879d9ce538c134cc5b71625dedb9c4cc8ce1a
Mandar Kulkarni <kulkarnimandar702@gmail.com>
2026-03-03 22:01:06 +0530

    final commit...

--- models/Todo.js
+++ models/Todo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose
   .connect(
-    "mongodb+srv://kulkarnimandar702:root@mymongodbcluster.2sfzk.mongodb.net/todo",
+    "mongodb+srv://kulkarnimandar702:root@mymongodbcluster.2sfzk.mongodb.net/todoAppExpress",
   )
   .then(() => console.log("Connected to MongoDB Successfully!"))
   .catch(() => console.log("Failed to Connect to MongoDB!"));
